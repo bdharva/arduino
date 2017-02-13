ccb631a1c634e2ec18bfec8fe0bee606b7de5777
bdharva <bdharva@alum.mit.edu>
2017-02-13 02:55:58 -0500

    Added intro tune

--- tones/tones.ino
+++ tones/tones.ino
@@ -44,10 +44,28 @@ void loop() {
     stringComplete = false;
     playNotes = true;
   }
+  if (playNotes == false) {
+    int melody[] = {
+      pitches[36], pitches[31], pitches[31], pitches[33], pitches[31], 0, pitches[35], pitches[36]
+    };
+    
+    // note durations: 4 = quarter note, 8 = eighth note, etc.:
+    int noteDurations[] = {
+      4, 8, 8, 4, 4, 4, 4, 4
+    };
+    for (int thisNote = 0; thisNote < (sizeof(melody)/sizeof(int)); thisNote++) {
+      int noteDuration = 1000 / noteDurations[thisNote];
+      tone(BUZ, melody[thisNote], noteDuration);
+      int pauseBetweenNotes = noteDuration * 1.30;
+      delay(pauseBetweenNotes);
+      noTone(BUZ);
+    }
+    delay(1000);
+  }
   if (playNotes == true) {
     int melody[] = {pitches[12*a+b], pitches[12*d+e]};
     int noteDurations[] = {c, f};
-    for (int thisNote = 0; thisNote < 2; thisNote++) {
+    for (int thisNote = 0; thisNote < (sizeof(melody)/sizeof(int)); thisNote++) {
       int noteDuration = 1000 / noteDurations[thisNote];
       tone(BUZ, melody[thisNote], noteDuration);
       int pauseBetweenNotes = noteDuration * 1.30;
